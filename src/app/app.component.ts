eb33e3867664013879eb2195c41a708f446cf51a
SangbumWoo <Sangbum.Woo@partner.bmw.co.kr>
2019-11-01 19:39:25 +0900

    Apply rxjs timer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { timer } from 'rxjs';
+import { take } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-root',
@@ -15,10 +18,25 @@ export class AppComponent implements OnInit {
 
     console.log('constructor()');
   }
+
   ngOnInit(): void {
     // throw new Error("Method not implemented.");
     console.log('ngOnInit()');
-    this.router.navigateByUrl('/list');
+    // this.router.navigateByUrl('/list');
+
+    timer(3000, 2000)
+      .subscribe(x => {
+        if (x % 2 == 0) {
+          console.log('timer: even ' + x);
+          this.router.navigateByUrl('/');
+        } else {
+          console.log('timer: odd ' + x);
+          this.router.navigateByUrl('/list');
+        }
+      })
+
+
+
   }
   title = 'angular-client';
 }
