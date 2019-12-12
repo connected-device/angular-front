d776219df8690e9789e3f6a54d71678d7c899b64
Sangbum Woo <sbwoo87@gmail.com>
2019-12-13 07:40:45 +0900

    reducer 오류

--- src/app/customers/state/customer.reducer.ts
+++ src/app/customers/state/customer.reducer.ts
@@ -28,63 +28,63 @@
 //   }
 // }
 
-import * as customerActions from "./customer.actions";
-import { Customer } from "../customer.model";
-import * as fromRoot from "../../state/app-state";
+import * as customerActions from './customer.actions';
+import { Customer } from '../customer.model';
+import * as fromRoot from '../../state/app-state';
 // import { Action } from 'rxjs/internal/scheduler/Action';
 // import { State, ActionsSubject } from '@ngrx/store';
 
 export interface CustomerState {
-  customers: Customer[];
-  loading: boolean;
-  loaded: boolean;
-  error: string;
+    customers: Customer[];
+    loading: boolean;
+    loaded: boolean;
+    error: string;
 }
 
 export interface AppState extends fromRoot.AppState {
-  customers: CustomerState;
+    customers: CustomerState;
 }
 
 export const initialState: CustomerState = {
-  customers: [],
-  loading: false,
-  loaded: false,
-  error: ""
+    customers: [],
+    loading: false,
+    loaded: false,
+    error: ''
 };
 
 export function customerReducer(
-  state = initialState,
-  action: customerActions.Action
+    state = initialState,
+    action: customerActions.Action
 ): CustomerState {
-  switch (action.type) {
-    case customerActions.CustomerActionTypes.LOAD_CUSTOMERS: {
-      return {
-        ...state,
-        loading: true
-      };
-    }
+    switch (action.type) {
+        case customerActions.CustomerActionTypes.LOAD_CUSTOMERS: {
+            return {
+                ...state,
+                loading: true
+            };
+        }
 
-    case customerActions.CustomerActionTypes.LOAD_CUSTOMERS_SUCCESS: {
-      return {
-        ...state,
-        loading: false,
-        loaded: true,
-        customers: action.payload
-      };
-    }
+        case customerActions.CustomerActionTypes.LOAD_CUSTOMERS_SUCCESS: {
+            return {
+                ...state,
+                loading: false,
+                loaded: true,
+                customers: action.payload
+            };
+        }
 
-    case customerActions.CustomerActionTypes.LOAD_CUSTOMERS: {
-      return {
-        ...state,
-        customers: [],
-        loading: false,
-        loaded: false,
-        error: action.payload
-      };
-    }
+        case customerActions.CustomerActionTypes.LOAD_CUSTOMERS_FAIL: {
+            return {
+                ...state,
+                customers: [],
+                loading: false,
+                loaded: false,
+                error: action.payload
+            };
+        }
 
-    default: {
-      return state;
+        default: {
+            return state;
+        }
     }
-  }
 }
