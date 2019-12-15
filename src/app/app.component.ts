e7909ce84f010954a7872c6e03a4a32052acd3d0
Sangbum Woo <sbwoo87@gmail.com>
2019-12-15 19:34:38 +0900

    Add margin and nav link

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,60 +1,60 @@
-import { Component, OnInit } from "@angular/core";
-import { Router } from "@angular/router";
-import { FormGroup, FormControl, Validators } from "@angular/forms";
-import { OrganizationsService } from "./organizations/organizations-services/organizations.service";
-import { timer } from "rxjs";
-import { take } from "rxjs/operators";
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { OrganizationsService } from './organizations/organizations-services/organizations.service';
+import { timer } from 'rxjs';
+import { take } from 'rxjs/operators';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"]
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  /**
-   *
-   */
 
-  organizations: Object;
-  public form: FormGroup;
+    constructor(
+        private router: Router,
+        private organizationService: OrganizationsService
+    ) {
+        // super();
+        this.form = new FormGroup(
+            {
+                organizationId: new FormControl(null)
+            },
+            { updateOn: 'change' }
+        ); // blur, change, submit
+    }
+    /**
+     *
+     */
 
-  constructor(
-    private router: Router,
-    private organizationService: OrganizationsService
-  ) {
-    // super();
-    this.form = new FormGroup(
-      {
-        organizationId: new FormControl(null)
-      },
-      { updateOn: "change" }
-    ); // blur, change, submit
-  }
+    organizations: Object;
+    public form: FormGroup;
+    title = 'angular-client';
 
-  ngOnInit(): void {
-    // this.router.navigateByUrl("/users-edit/5dee52d07f7dc47a3a48f41e");
-    this.router.navigateByUrl("/users-list");
+    ngOnInit(): void {
+        // this.router.navigateByUrl("/users-edit/5dee52d07f7dc47a3a48f41e");
+        this.router.navigateByUrl('/users-list');
 
-    // timer(3000000, 2000)
-    //   .subscribe(x => {
-    //     if (x % 3 == 0) {
-    //       console.log('timer: ' + x);
-    //       this.router.navigateByUrl('/');
-    //     } else if (x % 3 == 1) {
-    //       console.log('timer: ' + x);
-    //       this.router.navigateByUrl('/list');
-    //     } else {
-    //       console.log('timer: ' + x);
-    //       this.router.navigateByUrl('/users');
-    //     }
-    //   })
-    this.list();
-  }
-  title = "angular-client";
+        // timer(3000000, 2000)
+        //   .subscribe(x => {
+        //     if (x % 3 == 0) {
+        //       console.log('timer: ' + x);
+        //       this.router.navigateByUrl('/');
+        //     } else if (x % 3 == 1) {
+        //       console.log('timer: ' + x);
+        //       this.router.navigateByUrl('/list');
+        //     } else {
+        //       console.log('timer: ' + x);
+        //       this.router.navigateByUrl('/users');
+        //     }
+        //   })
+        this.list();
+    }
 
-  list() {
-    this.organizationService.getOrganizations().subscribe(data => {
-      this.organizations = data;
-    });
-  }
+    list() {
+        this.organizationService.getOrganizations().subscribe(data => {
+            this.organizations = data;
+        });
+    }
 }
--- src/app/organizations/organizations-entity/organizations.ts
+++ src/app/organizations/organizations-entity/organizations.ts
@@ -1,9 +1,10 @@
 /* Defines the product entity */
 export interface Organization {
-  _id: string;
-  organizationId: string;
-  password: string;
-  organizationName: string;
-  group: string;
-  role: string;
+    _id: string;
+    organizationId: string;
+    password: string;
+    organizationName: string;
+    group: string;
+    role: string;
 }
+
