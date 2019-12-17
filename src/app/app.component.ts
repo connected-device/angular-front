e4b8ab1b3f1e01e07746faef2feb5fc7b420d2f9
SangbumWoo <Sangbum.Woo@partner.bmw.co.kr>
2019-12-17 17:32:18 +0900

    Organization dropdown default select.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,60 +1,71 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { OrganizationsService } from './organizations/organizations-services/organizations.service';
-import { timer } from 'rxjs';
-import { take } from 'rxjs/operators';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { OrganizationsService } from "./organizations/organizations-services/organizations.service";
+import { IOrganization } from "./organizations/organizations-entity/organizations";
+import { AppService } from "./app.service";
+import { timer } from "rxjs";
+import { take } from "rxjs/operators";
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent implements OnInit {
+  organizations: IOrganization[];
+  constructor(
+    private router: Router,
+    private organizationService: OrganizationsService,
+    private appService: AppService
+  ) {
+    appService.organizationId = "aa";
+    this.form = new FormGroup(
+      {
+        organization: new FormControl(null)
+      },
+      { updateOn: "change" }
+    ); // blur, change, submit
 
-    constructor(
-        private router: Router,
-        private organizationService: OrganizationsService
-    ) {
-        // super();
-        this.form = new FormGroup(
-            {
-                organizationId: new FormControl(null)
-            },
-            { updateOn: 'change' }
-        ); // blur, change, submit
-    }
-    /**
-     *
-     */
+    this.organizationService.getOrganizations().subscribe(data => {
+      this.organizations = data;
+      const index: number = this.organizations.findIndex(
+        x => x.organizationId === appService.organizationId
+      );
 
-    organizations: Object;
-    public form: FormGroup;
-    title = 'angular-client';
+      this.form.controls.organization.patchValue(this.organizations[index]);
+    });
+  }
 
-    ngOnInit(): void {
-        // this.router.navigateByUrl("/users-edit/5dee52d07f7dc47a3a48f41e");
-        this.router.navigateByUrl('/users-list');
+  public form: FormGroup;
+  title = "angular-client";
 
-        // timer(3000000, 2000)
-        //   .subscribe(x => {
-        //     if (x % 3 == 0) {
-        //       console.log('timer: ' + x);
-        //       this.router.navigateByUrl('/');
-        //     } else if (x % 3 == 1) {
-        //       console.log('timer: ' + x);
-        //       this.router.navigateByUrl('/list');
-        //     } else {
-        //       console.log('timer: ' + x);
-        //       this.router.navigateByUrl('/users');
-        //     }
-        //   })
-        this.list();
-    }
+  ngOnInit(): void {
+    // this.router.navigateByUrl("/users-edit/5dee52d07f7dc47a3a48f41e");
+    this.router.navigateByUrl("/users-list");
 
-    list() {
-        this.organizationService.getOrganizations().subscribe(data => {
-            this.organizations = data;
-        });
-    }
+    // timer(3000000, 2000)
+    //   .subscribe(x => {
+    //     if (x % 3 == 0) {
+    //       console.log('timer: ' + x);
+    //       this.router.navigateByUrl('/');
+    //     } else if (x % 3 == 1) {
+    //       console.log('timer: ' + x);
+    //       this.router.navigateByUrl('/list');
+    //     } else {
+    //       console.log('timer: ' + x);
+    //       this.router.navigateByUrl('/users');
+    //     }
+    //   })
+
+    // this.list();
+  }
+
+  // list() {
+  //   this.organizationService.getOrganizations().subscribe(data => {
+  //     this.organizations = data;
+  //     // this.form.controls.orders.patchValue(this.orders[0].id);
+  //     this.form.controls.organization.patchValue(this.organizations[2]);
+  //   });
+  // }
 }
--- src/app/organizations/organizations-services/organizations.service.ts
+++ src/app/organizations/organizations-services/organizations.service.ts
@@ -4,36 +4,38 @@ import { environment } from "../../../environments/environment";
 import { IOrganization } from "../organizations-entity/organizations";
 
 @Injectable({
-    providedIn: "root"
+  providedIn: "root"
 })
 export class OrganizationsService {
-    constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-    getOrganizations() {
-        return this.http.get<IOrganization[]>(`${environment.apiUrl}/organizations`);
-    }
+  getOrganizations() {
+    return this.http.get<IOrganization[]>(
+      `${environment.apiUrl}/organizations`
+    );
+  }
 
-    getOrganization(_id: string) {
-        return this.http.get<IOrganization>(
-            `${environment.apiUrl}/organizations/${_id}`
-        );
-    }
+  getOrganization(_id: string) {
+    return this.http.get<IOrganization>(
+      `${environment.apiUrl}/organizations/${_id}`
+    );
+  }
 
-    findByOrganizationId(organizationId: string) {
-        return this.http.get(
-            `${environment.apiUrl}/organizations/findByOrganizationId?organizationId=${organizationId}`
-        );
-    }
+  findByOrganizationId(organizationId: string) {
+    return this.http.get(
+      `${environment.apiUrl}/organizations/findByOrganizationId?organizationId=${organizationId}`
+    );
+  }
 
-    postOrganizations(data: IOrganization) {
-        return this.http.post(`${environment.apiUrl}/organizations`, data);
-    }
+  postOrganizations(data: IOrganization) {
+    return this.http.post(`${environment.apiUrl}/organizations`, data);
+  }
 
-    putOrganizations(_id: string, data: IOrganization) {
-        return this.http.put(`${environment.apiUrl}/organizations/${_id}`, data);
-    }
+  putOrganizations(_id: string, data: IOrganization) {
+    return this.http.put(`${environment.apiUrl}/organizations/${_id}`, data);
+  }
 
-    deleteOrganization(id: string) {
-        return this.http.delete(`${environment.apiUrl}/organizations/${id}`);
-    }
+  deleteOrganization(id: string) {
+    return this.http.delete(`${environment.apiUrl}/organizations/${id}`);
+  }
 }
