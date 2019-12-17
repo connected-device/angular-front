27e5b63e89098cebd4e4b59a3095f0bb4c28f0e3
Sangbum Woo <sbwoo87@gmail.com>
2019-12-18 07:48:50 +0900

    change.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,78 +1,78 @@
-import { Component, OnInit } from "@angular/core";
-import { Router } from "@angular/router";
-import { FormGroup, FormControl, Validators } from "@angular/forms";
-import { OrganizationsService } from "./organizations/organizations-services/organizations.service";
-import { IOrganization } from "./organizations/organizations-entity/organizations";
-import { AppService } from "./app.service";
-import { timer } from "rxjs";
-import { take } from "rxjs/operators";
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { OrganizationsService } from './organizations/organizations-services/organizations.service';
+import { IOrganization } from './organizations/organizations-entity/organizations';
+import { AppService } from './app.service';
+import { timer } from 'rxjs';
+import { take } from 'rxjs/operators';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"]
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  organizations: IOrganization[];
-  constructor(
-    private router: Router,
-    private organizationService: OrganizationsService,
-    private appService: AppService
-  ) {
-    appService.organizationId = "aa";
-    this.form = new FormGroup(
-      {
-        organization: new FormControl(null)
-      },
-      { updateOn: "change" }
-    ); // blur, change, submit
+    organizations: IOrganization[];
+    constructor(
+        private router: Router,
+        private organizationService: OrganizationsService,
+        private appService: AppService
+    ) {
+        appService.organizationId = 'aa2';
+        this.form = new FormGroup(
+            {
+                organization: new FormControl(null)
+            },
+            { updateOn: 'change' }
+        ); // blur, change, submit
 
-    this.organizationService.getOrganizations().subscribe(data => {
-      this.organizations = data;
-      const index: number = this.organizations.findIndex(
-        x => x.organizationId === appService.organizationId
-      );
+        this.organizationService.getOrganizations().subscribe(data => {
+            this.organizations = data;
+            const index: number = this.organizations.findIndex(
+                x => x.organizationId === appService.organizationId
+            );
 
-      this.form.controls.organization.patchValue(this.organizations[index]);
-    });
-  }
+            this.form.controls.organization.patchValue(this.organizations[index]);
+        });
+    }
 
-  public form: FormGroup;
-  title = "angular-client";
+    public form: FormGroup;
+    title = 'angular-client';
 
-  ngOnInit(): void {
-    // this.router.navigateByUrl("/users-edit/5dee52d07f7dc47a3a48f41e");
-    this.router.navigateByUrl("/users-list");
+    ngOnInit(): void {
+        // this.router.navigateByUrl("/users-edit/5dee52d07f7dc47a3a48f41e");
+        this.router.navigateByUrl('/users-list');
 
-    // timer(3000000, 2000)
-    //   .subscribe(x => {
-    //     if (x % 3 == 0) {
-    //       console.log('timer: ' + x);
-    //       this.router.navigateByUrl('/');
-    //     } else if (x % 3 == 1) {
-    //       console.log('timer: ' + x);
-    //       this.router.navigateByUrl('/list');
-    //     } else {
-    //       console.log('timer: ' + x);
-    //       this.router.navigateByUrl('/users');
-    //     }
-    //   })
+        // timer(3000000, 2000)
+        //   .subscribe(x => {
+        //     if (x % 3 == 0) {
+        //       console.log('timer: ' + x);
+        //       this.router.navigateByUrl('/');
+        //     } else if (x % 3 == 1) {
+        //       console.log('timer: ' + x);
+        //       this.router.navigateByUrl('/list');
+        //     } else {
+        //       console.log('timer: ' + x);
+        //       this.router.navigateByUrl('/users');
+        //     }
+        //   })
 
-    // this.list();
-  }
+        // this.list();
+    }
 
-  // list() {
-  //   this.organizationService.getOrganizations().subscribe(data => {
-  //     this.organizations = data;
-  //     // this.form.controls.orders.patchValue(this.orders[0].id);
-  //     this.form.controls.organization.patchValue(this.organizations[2]);
-  //   });
-  // }
+    // list() {
+    //   this.organizationService.getOrganizations().subscribe(data => {
+    //     this.organizations = data;
+    //     // this.form.controls.orders.patchValue(this.orders[0].id);
+    //     this.form.controls.organization.patchValue(this.organizations[2]);
+    //   });
+    // }
 
-  onChange(event) {
-    // console.log("event : ", event);
-    let organization = this.form.controls.organization.value;
-    console.log("selected organization--->", organization);
-    this.appService.organizationId = organization.organizationId;
-  }
+    onChange(event) {
+        // console.log("event : ", event);
+        const organization = this.form.controls.organization.value;
+        console.log('selected organization : ', organization);
+        this.appService.organizationId = organization.organizationId;
+    }
 }
