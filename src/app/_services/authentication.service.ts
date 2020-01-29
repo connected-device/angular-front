18855b280d3bab8cc904697e8b91c565cb6b1b26
SangbumWoo <Sangbum.Woo@partner.bmw.co.kr>
2020-01-29 18:05:46 +0900

    change

--- src/app/_services/authentication.service.ts
+++ src/app/_services/authentication.service.ts
@@ -30,6 +30,7 @@ export class AuthenticationService {
       })
       .pipe(
         map(user => {
+          console.log("user", user);
           // login successful if there's a jwt token in the response
           if (user && user.token) {
             // store user details and jwt token in local storage to keep user logged in between page refreshes
